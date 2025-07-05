151ef0e4db6334a4d839c3c3cc3864d4ba23a39e
Zanal Sorathiya <zanalsorathiya@gmail.com>
2025-07-05 08:14:51 +0530

    Update dijkstra.hpp

--- dynamic-transport-optimizer/backend/src/dijkstra.hpp
+++ dynamic-transport-optimizer/backend/src/dijkstra.hpp
@@ -3,7 +3,6 @@
 #include "graph.hpp"
 #include <vector>
 #include <utility>
-
 std::pair<std::vector<int>, int> dijkstra(Graph& graph, int source, int target);
 void saveRouteAsJSON(Graph& graph, std::vector<int> path, int total_time);
 
